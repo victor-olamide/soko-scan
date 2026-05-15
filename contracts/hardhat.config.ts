d8541b9133d446348734c853103ff62d56fb9786
victor-olamide <olaboyevictor20@gmail.com>
2026-05-15 14:50:43 +0100

    chore(contracts): add Alfajores testnet to hardhat networks

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,7 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: { version: "0.8.20", settings: { optimizer: { enabled: true, runs: 200 } } },
+  networks: {
+    alfajores: { url: "https://alfajores-forno.celo-testnet.org", accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [], chainId: 44787 },
+  },
 };
 export default config;

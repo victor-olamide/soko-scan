c31a268639064d891fe9adef21bf86b14af2edb9
victor-olamide <olaboyevictor20@gmail.com>
2026-05-15 14:50:43 +0100

    feat(contracts): define Merchant data structure

--- contracts/contracts/SokoScan.sol
+++ contracts/contracts/SokoScan.sol
@@ -10,6 +10,16 @@ contract SokoScan is Ownable, ReentrancyGuard {
     IERC20 public immutable cUSD;
     SokoPoints public immutable sokoPoints;
 
+    struct Merchant {
+        address wallet;
+        string name;
+        string category;
+        bool active;
+        uint256 totalReceived;
+        uint256 txCount;
+        uint256 pointsPerCUSD;
+    }
+
     constructor(address _cUSD, address _sokoPoints) Ownable(msg.sender) {
         cUSD = IERC20(_cUSD);
         sokoPoints = SokoPoints(_sokoPoints);

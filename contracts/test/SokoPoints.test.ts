2ec29ac5ecf37055098f66e334e97c38a9519e85
victor-olamide <olaboyevictor20@gmail.com>
2026-05-15 14:55:47 +0100

    test(contracts): scaffold SokoPoints test suite

---
+++ contracts/test/SokoPoints.test.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("SokoPoints", function () {
+  it("should deploy with correct name and symbol", async function () {});
+});

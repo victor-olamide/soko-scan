79bbf5420e812d0c952fdc294cbbfae2704bb92e
victor-olamide <olaboyevictor20@gmail.com>
2026-05-15 14:50:43 +0100

    feat(contracts): add ReentrancyGuard and SokoPoints integration

--- contracts/contracts/SokoScan.sol
+++ contracts/contracts/SokoScan.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "./SokoPoints.sol";
 
-contract SokoScan is Ownable {
+contract SokoScan is Ownable, ReentrancyGuard {
     IERC20 public immutable cUSD;
+    SokoPoints public immutable sokoPoints;
 
-    constructor(address _cUSD) Ownable(msg.sender) {
+    constructor(address _cUSD, address _sokoPoints) Ownable(msg.sender) {
         cUSD = IERC20(_cUSD);
+        sokoPoints = SokoPoints(_sokoPoints);
     }
 }

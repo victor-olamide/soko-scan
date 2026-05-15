19ba7693c2b24d4eb989a43f72cdf5c08af44efa
victor-olamide <olaboyevictor20@gmail.com>
2026-05-15 14:50:43 +0100

    feat(contracts): add burn function for point redemption

--- contracts/contracts/SokoPoints.sol
+++ contracts/contracts/SokoPoints.sol
@@ -16,4 +16,8 @@ contract SokoPoints is ERC20, Ownable {
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
 }

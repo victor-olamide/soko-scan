4f9b99b8370913c1efb7228f16ebb58e2e1bc5b5
victor-olamide <olaboyevictor20@gmail.com>
2026-05-15 14:55:47 +0100

    test(contracts): add deploy and name/symbol assertion to SokoPoints test

--- contracts/test/SokoPoints.test.ts
+++ contracts/test/SokoPoints.test.ts
@@ -2,5 +2,16 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("SokoPoints", function () {
-  it("should deploy with correct name and symbol", async function () {});
+  async function deploy() {
+    const [owner, sokoScan, user] = await ethers.getSigners();
+    const SP = await ethers.getContractFactory("SokoPoints");
+    const sp = await SP.deploy();
+    return { sp, owner, sokoScan, user };
+  }
+
+  it("should deploy with correct name and symbol", async function () {
+    const { sp } = await deploy();
+    expect(await sp.name()).to.equal("SokoPoints");
+    expect(await sp.symbol()).to.equal("SOKO");
+  });
 });
